82fd45f5a8c6f76e7942893f9ec421be72df0a7e
pekarj <jepekar@ucsd.edu>
2018-12-19 10:29:16 -0500

    update help

--- patristic_distances_filtered.py
+++ patristic_distances_filtered.py
@@ -10,7 +10,7 @@ from treeswift import read_tree_newick
 # parse arguments
 def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', required=False, type=argparse.FileType('r'), default=stdin, help="Input FASTA")
+    parser.add_argument('-i', '--input', required=False, type=argparse.FileType('r'), default=stdin, help="Input tree")
     parser.add_argument('-o', '--output', required=False, type=argparse.FileType('w'), default=stdout, help="Output, don't include file type")
     parser.add_argument('-t', '--threshold', required=False, type=float, default=1, help="Threshold")
     args = parser.parse_args()
